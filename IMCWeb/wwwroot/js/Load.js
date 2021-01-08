9a661485bae37ded77fa4f17f8233add286d44ba
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2021-01-08 16:43:10 -0300

    .

---
+++ IMCWeb/wwwroot/js/Load.js
@@ -0,0 +1,9 @@
+﻿async function LoadView(controller, action, divIdentifier) {
+
+    const clientUrl = `/${controller}/${action}`;
+
+    const page = await fetch(clientUrl);
+    const pageHtml = await page.text();
+
+    document.getElementById(divIdentifier).innerHTML = pageHtml;
+}
